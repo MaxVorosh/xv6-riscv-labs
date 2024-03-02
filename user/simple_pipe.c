0d8c5d83f2b39ec0737a993739460f59eaf2d254
MaxVorosh <ma_voroshilov@mail.ru>
2024-03-02 04:52:41 -0500

    Change approach to sending arguments through pipe

--- user/simple_pipe.c
+++ user/simple_pipe.c
@@ -7,40 +7,39 @@ int
 main(int argc, char** argv) {
     int p[2];
     pipe(p);
-    const int buf_size = 100;
+    const int buf_size = 21;
     int pid = fork();
     if (pid < 0) {
-        int msg_len = 24;
+        const int msg_len = 24;
         write(2, "Can't start new process\n", msg_len);
         exit(1);
     }
     if (pid == 0) {
         close(p[1]);
         char buf[buf_size];
-        read(p[0], buf, buf_size);
-        printf("%s", buf);
+        while (read(p[0], buf, buf_size) > 0) {
+            printf("%s", buf);
+        }
         close(p[0]);
         exit(0);
     }
     close(p[0]);
     char buf[buf_size];
-    char* ptr = buf;
-    int sum_len = 0;
-    for (int i = 1; i < argc; ++i) {
-        strcpy(ptr, argv[i]);
-        int len = strlen(argv[i]) + 1;
-        sum_len += len;
-        ptr += len;
-    }
-    for (int i = 0; i < sum_len; ++i) {
-        if (buf[i] == '\0') {
-            buf[i] = '\n';
-        }
-    }
-    buf[sum_len] = '\0';
-    write(p[1], buf, sum_len);
-    close(p[1]);
     int status;
+    for (int i = 1; i < argc; ++i) {
+        int len = strlen(argv[i]);
+        if (len >= buf_size) {
+            close(p[1]);
+            wait(&status);
+            const int msg_len = 16;
+            write(2, "Buffer overflow\n", msg_len);
+            exit(2);
+        }
+        strcpy(buf, argv[i]);
+        buf[len] = '\n';
+        write(p[1], buf, len + 1);
+    }
+    close(p[1]);
     wait(&status);
     exit(0);
 }
