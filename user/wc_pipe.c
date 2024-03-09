a655bb8a131ada7808ff8f697384b9e3e772a9a4
MaxVorosh <ma_voroshilov@mail.ru>
2024-03-09 14:50:48 -0500

    Check error in pipe and remove buffer from writing in pipe

--- user/wc_pipe.c
+++ user/wc_pipe.c
@@ -6,8 +6,11 @@
 int
 main(int argc, char** argv) {
     int p[2];
-    pipe(p);
-    const int buf_size = 21;
+    if (pipe(p) < 0) {
+        const int msg_len = 18;
+        write(2, "Can't create pipe\n", msg_len);
+        exit(5);
+    }
     int pid = fork();
     if (pid < 0) {
         const int msg_len = 24;
@@ -26,20 +29,11 @@ main(int argc, char** argv) {
         exit(3);
     }
     close(p[0]);
-    char buf[buf_size];
     int status;
     for (int i = 1; i < argc; ++i) {
         int len = strlen(argv[i]);
-        if (len >= buf_size) {
-            close(p[1]);
-            wait(&status);
-            const int msg_len = 16;
-            write(2, "Buffer overflow\n", msg_len);
-            exit(2);
-        }
-        strcpy(buf, argv[i]);
-        buf[len] = '\n';
-        write(p[1], buf, len + 1);
+        argv[i][len] = '\n';
+        write(p[1], argv[i], len + 1);
     }
     close(p[1]);
     wait(&status);
